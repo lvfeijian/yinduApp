61c75c418911d859e72e04766aceaa61fe61ccbd
lvjianfei <lvjianfei@talk-fun.com>
2021-10-24 14:59:59 +0800

    fix: 我的页面完成

--- src/views/mine/Mine.vue
+++ src/views/mine/Mine.vue
@@ -1,7 +1,88 @@
 <!--我的-->
 <template>
-  <div>
-    我的
+  <div class="mine">
+    <div class="title">
+      ME
+    </div>
+    <div class="bg">
+    </div>
+    <div class="card">
+      <div class="left">
+        <div class="head_image">
+          <!-- <img src="" alt="" /> -->
+        </div>
+        <div class="tips">MY DATA</div>
+      </div>
+      <div class="right">
+        <div class="name">JENNY</div>
+        <div class="code">
+          <div class="text">INVITATION CODE：239987</div>
+          <div class="copy">COPY</div>
+        </div>
+        <div class="level">VIP LEVEL：GOLD</div>
+        <div class="time">THE REMAINING NUMBER OF DAYS：18 DAY</div>
+      </div>
+    </div>
+    <div class="navbar">
+      <div class="item">
+        <div class="icon">
+          <img src="@/assets/img/mine/show_goumai_fill.png" alt="" />
+        </div>
+        <p>PURCHASE</p>
+      </div>
+      <div class="item">
+        <div class="icon">
+          <img src="@/assets/img/mine/tuandui.png" alt="" />
+        </div>
+        <p>TEAM</p>
+      </div>
+      <div class="item">
+        <div class="icon">
+          <img src="@/assets/img/mine/tixian.png" alt="" />
+        </div>
+        <p>PROFIT</p>
+      </div>
+      <div class="item">
+        <div class="icon">
+          <img src="@/assets/img/mine/tuandui.png" alt="" />
+        </div>
+        <p>WALLET</p>
+      </div>
+    </div>
+    <div class="list">
+      <div class="list_item">
+        <img class="icon" src="../../assets/img/mine/goumaicantuan.png" alt="" />
+        <div class="list_name">SUBORDINATE PURCHASE</div>
+        <img class="jiantou" src="../../assets/img/mine/goumaicantuan.png" alt="" />
+      </div>
+      <div class="list_item">
+        <img class="icon" src="../../assets/img/mine/goumaicantuan.png" alt="" />
+        <div class="list_name">PLATFORM ANNOUNCEMENT</div>
+      </div>
+      <div class="list_item">
+        <img class="icon" src="../../assets/img/mine/xiazai.png" alt="" />
+        <div class="list_name">APP DOWNLOAD</div>
+      </div>
+      <div class="list_item">
+        <img class="icon" src="../../assets/img/mine/zaixiankefu.png" alt="" />
+        <div class="list_name">ONLINE SERVICE</div>
+      </div>
+      <div class="list_item">
+        <img class="icon" src="../../assets/img/mine/xiugaimima.png" alt="" />
+        <div class="list_name">CHANGE PASSWORD</div>
+      </div>
+      <div class="list_item">
+        <img class="icon" src="../../assets/img/mine/yinhangqia.png" alt="" />
+        <div class="list_name">BANK CARD</div>
+      </div>
+      <div class="list_item">
+        <img class="icon" src="../../assets/img/mine/bangzhuzhongxin.png" alt="" />
+        <div class="list_name">ABOUT US</div>
+      </div>
+    </div>
+    <div class="exit">
+      EXIT THE SYSTEM
+    </div>
   </div>
 </template>
 
@@ -16,7 +97,8 @@
 
     computed: {},
 
-    mounted() { },
+    mounted() {
+    },
 
     methods: {}
   }
