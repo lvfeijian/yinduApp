a4395eaa39502a8ef46815af607a74e021ece305
zzy <1936308757@qq.com>
2021-10-27 01:48:49 +0800

    支付功能添加

--- src/views/member/Member.vue
+++ src/views/member/Member.vue
@@ -12,16 +12,32 @@
 			</div>
 			<!-- 积分版列表 -->
 			<div class="score-item" v-for="(item,index) in vipList" :key="index">
-				<img :src="require('../../assets/img/home/' + item.img)"/>
+				<img :src="require('../../assets/img/home/' + item.img)" />
 				<div class="score-title">{{item.title}}</div>
-				<div class="score-btn">
-					<div class="score-money">{{item.price}}</div>
+				<div class="score-btn" :class="{'active':item.select == true}" @click="onItemList(item)">
+					{{item.price}}
 				</div>
 			</div>
 			<div class="pay-btn" @click="onPayBtn()">
 				<div class="pay-title">PAY</div>
 			</div>
 		</div>
+		<van-popup v-model="show" round position="bottom" :style="{ height: '33%' }">
+			<div class="pay-box">
+				<div class="pay-title">PAY IMMEDIATELY</div>
+				<div class="pay-container">
+					<div class="wallet">
+						<img src="../../assets/img/home/wallet.png" alt="">
+					</div>
+					<div class="small-title">BANK CARD</div>
+					<div class="pay-check" @click="onCheck">
+						<van-icon v-if="showIcon" name="checked" size="23px" />
+					</div>
+				</div>
+				<div class="pay-btntwo" @clcik="onPatBtnTwo">PAY</div>
+			</div>
+
+		</van-popup>
 	</div>
 </template>
 
@@ -29,44 +45,56 @@
 	import Vue from 'vue';
 	import {
 		NavBar,
-		Dialog 
+		Dialog,
+		Popup,
+		Icon
 	} from 'vant';
 	Vue.use(NavBar);
 	Vue.use(Dialog);
+	Vue.use(Popup);
+	Vue.use(Icon);
 	export default {
 		data() {
 			return {
 				vipList: [{
 						img: "silver.png",
 						title: "SILVER VIP",
-						price: "₹600"
+						price: "₹600",
+						select: "false"
 					},
 					{
 						img: "gold.png",
 						title: "GOLD VIP",
-						price: "₹2600"
+						price: "₹2600",
+						select: "false"
 					},
 					{
 						img: "diamond.png",
 						title: "DIAMOND VIP",
-						price: "₹10000"
+						price: "₹10000",
+						select: "false"
 					},
 					{
 						img: "golden.png",
 						title: "GOLDEN DIAMOND VIP",
-						price: "₹25000"
+						price: "₹25000",
+						select: "false"
 					},
 					{
 						img: "black.png",
 						title: "BLACK DIAMOND VIP",
-						price: "₹50000"
+						price: "₹50000",
+						select: "false"
 					},
 					{
 						img: "grown.png",
 						title: "GROWN VIP",
-						price: "₹100000"
+						price: "₹100000",
+						select: "false"
 					},
-				]
+				],
+				show: false, // 弹窗显示
+				showIcon: false, // 图标选中
 			};
 		},
 
@@ -80,18 +108,49 @@
 			onClickLeft: function() {
 				this.$router.go(-1)
 			},
+
 			/**
-			 * 支付
+			 * 支付设置
 			 */
-			onPayBtn: function(){
+			onItemList: function(item) {
+				// this.show = true;
+				item.select = true
+				if(item.select){
+					item.select = false
+				}else{
+					item.select = true
+				}
+			},
+			/**
+			 * 升级会员支付
+			 */
+			onPayBtn: function() {
 				Dialog.alert({
-				  title: '标题',
-				  width: 264,
-				  message: '弹窗内容',
-				  theme: 'round-button',
+					title: '标题',
+					width: 264,
+					message: '弹窗内容',
+					theme: 'round-button',
 				}).then(() => {
-				  // on close
+					// on close
 				});
+			},
+			/**
+			 * 图标选中
+			 */
+			onCheck: function() {
+				if (this.showIcon) {
+					this.showIcon = false
+				} else {
+					this.showIcon = true
+				}
+
+			},
+
+			/**
+			 * 支付2
+			 */
+			onPatBtnTwo: function() {
+				console.log("支付2");
 			}
 		}
 	}
